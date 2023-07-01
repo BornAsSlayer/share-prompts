5e6dc7cb59f3f391f0a9551b58be8662efec3e78
Sujin S <57178220+BornAsSlayer@users.noreply.github.com>
2023-07-01 21:18:28 +0530

    authentication is done and db config is corrected an posting to db is verifed

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,20 +6,20 @@ import { useState, useEffect, useCallback } from 'react';
 import { signIn,signOut,useSession,getProviders } from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data : session } = useSession();
 
   const[providers, setProviders] = useState(null);
 
   const [ toggleDropDown, setToggleDropDown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   },[])
 
   return (
@@ -34,9 +34,9 @@ const Nav = () => {
         <p className='logo_text'>Promptopia</p>
       </Link>
 
-      {/*Mobile Navigation*/}
+      {/*Desktop Navigation*/}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href='/create-prompt' className='black_btn'>
               Create Post
@@ -45,7 +45,7 @@ const Nav = () => {
               Sign Out
             </button>
             <Link href='/profile'>
-              <Image src='/assets/images/logo.svg'
+              <Image src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
@@ -71,9 +71,9 @@ const Nav = () => {
 
       {/*Mobile Navigation*/}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
-          <Image src='/assets/images/logo.svg'
+          <Image src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
@@ -126,4 +126,4 @@ const Nav = () => {
   )
 }
 
-export default Nav
+export default Nav 
