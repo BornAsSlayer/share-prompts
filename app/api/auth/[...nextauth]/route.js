98d81f21670b8781b2e95ac385238dce2706892e
Sujin S <57178220+BornAsSlayer@users.noreply.github.com>
2023-07-01 16:25:35 +0530

    added user sesson management

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
 
 import { connectToDB } from "@utils/database";
+import User from '@Model/User';
 
 console.log({
     clientId: process.env.GOOGLE_ID,
@@ -16,13 +17,33 @@ const handler = NextAuth({
         })
     ],
     async session ({ session }){
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
+        session.user.id = sessionUser._id.toString();
 
+        return session;
     },
     async signIn({ profile }){
         try {
-            
+            await connectToDB();
+            const userExists = await User.findOne({
+                email: profile.email
+            });
+
+            if(!userExists){
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ","").
+                    toLowerCase(),
+                    image: profile.picture
+                })
+            }
+
+            return true;
         } catch (error) {
-            
+            console.log(error)
+            return false;
         }
     },
 })
