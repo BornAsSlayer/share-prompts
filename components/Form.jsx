b47b8cea4c274906e774b4101f1b0d26c5b8e6b9
Sujin S <57178220+BornAsSlayer@users.noreply.github.com>
2023-07-01 22:06:19 +0530

    added base for Forms

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import Link from 'next/link';
 
 const Form = ({type, post, setPost, submitting, handleSubmit}) => {
   return (
-    <div>form</div>
+    <section className='w-full max-w-full flex-start flex-col'>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{type} Post</span>
+        </h1>
+        <p className='desc text-left max-w-md'>
+          {type} and share amazing prompts with the world and let your imagination wild with
+          any AI tool.
+        </p>
+        <form
+         onSubmit={handleSubmit}
+         className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
+        >
+          <label>
+            <span className="font-satoshi font-semibold text-base text-gray-700">
+              Your AI Prompt 
+            </span>
+            
+          </label>
+        </form>
+    </section>
   )
 }
 
