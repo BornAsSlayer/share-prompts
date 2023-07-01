c12c9df77ba993af6052a04b605cf10c1355c704
Sujin S <57178220+BornAsSlayer@users.noreply.github.com>
2023-07-01 09:55:01 +0530

    nav-bar for mobile optimized

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,13 +2,15 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { signIn,signOut,useSession,getProviders } from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLogged = true;
+  const isUserLoggedIn = true;
 
   const[providers, setProviders] = useState(null);
+
+  const [ toggleDropDown, setToggleDropDown] = useState(false);
 
   useEffect(() => {
     const setProviders = async () => {
@@ -21,7 +23,7 @@ const Nav = () => {
   },[])
 
   return (
-    <div className='flex-between w-full mb-16 pt-3'>
+    <nav className='flex-between w-full mb-16 pt-3'>
       <Link href="/" className='flex gap-2 flex-center'>
         <Image src="/assets/images/logo.svg"
         alt='Promptopia Logo'
@@ -34,7 +36,7 @@ const Nav = () => {
 
       {/*Mobile Navigation*/}
       <div className='sm:flex hidden'>
-        {isUserLogged ? (
+        {isUserLoggedIn ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href='/create-prompt' className='black_btn'>
               Create Post
@@ -47,9 +49,7 @@ const Nav = () => {
               width={37}
               height={37}
               className="rounded-full"
-              alt='profile'>
-
-              </Image>
+              alt='profile'/>
             </Link>
           </div>
         ) : (
@@ -68,7 +68,61 @@ const Nav = () => {
           </>
         )}
       </div>
-    </div>
+
+      {/*Mobile Navigation*/}
+      <div className='sm:hidden flex relative'>
+        {isUserLoggedIn ? (
+          <div className='flex'>
+          <Image src='/assets/images/logo.svg'
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt='profile'
+              onClick={()=> setToggleDropDown ((prev) => !prev) }
+          />
+          {toggleDropDown && (
+            <div className='dropdown'>
+              <Link href="/profile"
+              className='dropdown_1'
+              onClick={() => setToggleDropDown(false)}>
+                My Profile
+              </Link>
+              <Link href="/create-prompt"
+              className='dropdown_1'
+              onClick={() => setToggleDropDown(false)
+              }>
+                Create Prompt
+              </Link>
+              <button
+              type='button'
+              onClick={() => {
+                setToggleDropDown(false);
+                signOut();
+              }}
+              className="mt-5 w-full black_btn"
+              >
+                Sign Out 
+              </button>
+            </div>
+          )}
+          </div>
+        ) : (
+          <>
+            {providers && 
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='black_btn'
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
   )
 }
 
